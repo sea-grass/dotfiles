4c9e28e82b4b12d1a30441510ffd5a9cc31ff643
sea-grass <dev.seagrass@gmail.com>
2025-05-25 00:56:06 -0400

    build.zig runs executable instead

--- build.zig
+++ build.zig
@@ -3,10 +3,6 @@ const mem = std.mem;
 const std = @import("std");
 const WriteFile = std.Build.Step.WriteFile;
 
-fn writeDots(dot_out: *std.Build.Step.WriteFile) void {
-    nvim.dots(dot_out);
-}
-
 pub fn build(b: *std.Build) void {
     const bs = .{
         .dots = b.step("dots", "Write dotfiles to dot_out"),
@@ -14,38 +10,16 @@ pub fn build(b: *std.Build) void {
 
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    _ = target;
-    _ = optimize;
 
-    const wf = b.addWriteFiles();
+    const exe = b.addExecutable(.{
+        .name = "link-dots",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
 
-    writeDots(wf);
+    b.getInstallStep().dependOn(&exe.step);
 
-    const x = b.addInstallDirectory(
-        .{
-            .source_dir = wf.getDirectory(),
-            .install_subdir = "dot_out",
-            .install_dir = .prefix,
-        },
-    );
-
-    bs.dots.dependOn(&wf.step);
-    bs.dots.dependOn(&x.step);
-}
-
-const nvim = struct {
-    pub fn dots(wf: *WriteFile) void {
-        inline for (&.{
-            .{ "dots/nvim/coc-settings.json", ".config/nvim/coc-settings.json" },
-            .{ "dots/nvim/init.lua", ".config/nvim/init.lua" },
-        }) |entry| {
-            const file_path, const out_path = entry;
-            write(wf, @embedFile(file_path), out_path);
-        }
-    }
-};
-
-fn write(wf: *WriteFile, bytes: []const u8, sub_path: []const u8) void {
-    debug.assert(mem.startsWith(u8, sub_path, ".config/"));
-    _ = wf.add(sub_path, bytes);
+    const run_exe = b.addRunArtifact(exe);
+    bs.dots.dependOn(&run_exe.step);
 }
