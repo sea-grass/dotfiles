ccc3bc82e5d1308173f57bedd7a65eef23191dbb
sea-grass <dev.seagrass@gmail.com>
2025-05-25 01:28:40 -0400

    fix(init): shellcheck fixes

--- init.bash
+++ init.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # dotfiles/init.bash
-root=$(cd -- "$(dirname -- '${BASH_SOURCE[0]}')" &> /dev/null && pwd)
+root=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 dots="$root/dots"
 
 error() {
@@ -76,40 +76,48 @@ apt_install() {
   fi
 }
 
-for section in $(find "$dots" -mindepth 1 -maxdepth 1 -type d); do
-  for dir_command in $(find "$section" -type f -name '*.dir'); do
+while IFS= read -r -d '' section; do
+  while IFS= read -r -d '' dir_command; do
+    # shellcheck disable=SC2088 # Tilde should not be expanded
     pat='~/(.+)'
     dir_data=$(head -1 "$dir_command")
+
     [[ "$dir_data" =~ $pat ]] || error "dir [$section/$dir_command] has malformed contents"
     dir="$HOME/${BASH_REMATCH[1]}"
+
     direxists "$dir"
-  done
-  for link_command in $(find "$section" -type f -name '*.link'); do
+  done < <(find "$section" -type f -name '*.dir' -print0)
+
+  while IFS= read -r -d '' link_command; do
     pat='(.+)->~/(.+)'
     link_data=$(head -1 "$link_command")   
+
     [[ "$link_data" =~ $pat ]] || error "link [$section/$link_command] has malformed contents"
     target="$section/${BASH_REMATCH[1]}"
     link_name="$HOME/${BASH_REMATCH[2]}"
     
     link "$target" "$link_name"
-  done
-  for download_command in $(find "$section" -type f -name '*.download'); do
+  done < <(find "$section" -type f -name '*.link' -print0)
+
+  while IFS= read -r -d '' download_command; do
     pat='(.+)->~/(.+)'
     download_data=$(head -1 "$download_command")
+
     [[ "$download_data" =~ $pat ]] || error "download [$section/$download_command] has malformed contents"
     url="${BASH_REMATCH[1]}"
     destination_file="$HOME/${BASH_REMATCH[2]}"
 
     download "$url" "$destination_file"
-  done
-  for apt_command in $(find "$section" -type f -name '*.apt'); do
+  done < <(find "$section" -type f -name '*.download')
+
+  while IFS= read -r -d '' apt_command; do
     if ! { type apt-get 1>/dev/null 2>&1; }; then
       info "apt-get is not present on this system. Not installing dependencies for [$section/$apt_command]"
     else
-      apt_data=( $(cat "$apt_command") )
-      for package in $apt_data; do
+      mapfile -t apt_data < <(cat "$apt_command")
+      for package in "${apt_data[@]}"; do
         apt_install "$package"
       done
     fi
-  done
-done
+  done < <(find "$section" -type f -name '*.apt')
+done < <(find "$dots" -mindepth 1 -maxdepth 1 -type d -print0)
